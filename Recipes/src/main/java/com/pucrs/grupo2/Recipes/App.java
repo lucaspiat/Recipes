67297993859d31a76912372e48bd2d1b30bf7d19
Lucas Piatnicki <lucas.piatnicki@edu.pucrs.br>
2020-06-29 16:49:22 -0300

    Minor fixes, created welcome and index hmtl pages

--- Recipes/src/main/java/com/pucrs/grupo2/Recipes/App.java
+++ Recipes/src/main/java/com/pucrs/grupo2/Recipes/App.java
@@ -3,14 +3,15 @@ package com.pucrs.grupo2.Recipes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
+/*import org.springframework.context.ConfigurableApplicationContext;*/
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        /*ConfigurableApplicationContext context = */
+        SpringApplication.run(App.class, args);
 
-        context.close();
+        /*context.close();*/
     }
 }
